f4f9e3b35ad0654f0293869269643787f50674da
Polina <debchuk_polina@mail.ru>
2022-11-17 12:41:36 -0500

    Simple Calculator with two Activities: advanced and common

---
+++ app/src/main/java/com/example/assignment1_simple_calculator/AdvanceVersion.java
@@ -0,0 +1,127 @@
+package com.example.assignment1_simple_calculator;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdvanceVersion extends AppCompatActivity implements View.OnClickListener {
+    TextView resultText;
+    Button n0;
+    Button n1;
+    Button n2;
+    Button n3;
+    Button n4;
+    Button n5;
+    Button n6;
+    Button n7;
+    Button n8;
+    Button n9;
+    Button clearBut;
+    Button equalBut;
+    Button addBut;
+    Button subBut;
+    Button timBut;
+    Button divBut;
+    Button standardButton;
+    TextView historyText;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advanceversion);
+        Log.d("Calculator App", "Advance Version Activity Created");
+
+        resultText = findViewById(R.id.showResText);
+        n0 = findViewById(R.id.num0);
+        n1 = findViewById(R.id.num1);
+        n2 = findViewById(R.id.num2);
+        n3 = findViewById(R.id.num3);
+        n4 = findViewById(R.id.num4);
+        n5 = findViewById(R.id.num5);
+        n6 = findViewById(R.id.num6);
+        n7 = findViewById(R.id.num7);
+        n8 = findViewById(R.id.num8);
+        n9 = findViewById(R.id.num9);
+        clearBut = findViewById(R.id.clearButton);
+        equalBut = findViewById(R.id.equalButton);
+        addBut = findViewById(R.id.addButton);
+        subBut = findViewById(R.id.subButton);
+        timBut = findViewById(R.id.timButton);
+        divBut = findViewById(R.id.divButton);
+        standardButton = findViewById(R.id.toStandardButton);
+        historyText = findViewById(R.id.showHistoryText);
+
+        n0.setOnClickListener(this);
+        n1.setOnClickListener(this);
+        n2.setOnClickListener(this);
+        n3.setOnClickListener(this);
+        n4.setOnClickListener(this);
+        n5.setOnClickListener(this);
+        n6.setOnClickListener(this);
+        n7.setOnClickListener(this);
+        n8.setOnClickListener(this);
+        n9.setOnClickListener(this);
+        clearBut.setOnClickListener(this);
+        equalBut.setOnClickListener(this);
+        addBut.setOnClickListener(this);
+        subBut.setOnClickListener(this);
+        timBut.setOnClickListener(this);
+        divBut.setOnClickListener(this);
+        standardButton.setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        String symbol;
+         switch (view.getId()) {
+            case R.id.clearButton:
+                CalculatorClass.history = "";
+                resultText.setText(CalculatorClass.history);
+                CalculatorClass.historyArrayList.clear();
+                break;
+            case R.id.toStandardButton:
+                Intent secondIntent = new Intent(AdvanceVersion.this, MainActivity.class);
+                startActivity(secondIntent);
+                break;
+            default:
+                symbol = ((Button) view).getText().toString();
+                if (CalculatorClass.isNumber(symbol)) {
+                    CalculatorClass.push(symbol);
+                    resultText.setText(CalculatorClass.history);
+                } else {
+                    if (CalculatorClass.history.isEmpty()) {
+                        Toast.makeText(AdvanceVersion.this, "First symbol have to be a number", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String previousSymbol = String.valueOf(CalculatorClass.history.charAt(CalculatorClass.history.length() - 1));
+                        System.out.println("previous symbol = " + previousSymbol);
+                        if (CalculatorClass.isOperator(previousSymbol)) {
+                            Toast.makeText(AdvanceVersion.this, "You cannot use two operators in a row, enter a number", Toast.LENGTH_SHORT).show();
+                        } else {
+                            CalculatorClass.push(symbol);
+                            if (symbol.equals("=")) {
+                                CalculatorClass.calculate();
+                                resultText.setText(CalculatorClass.history);
+                                CalculatorClass.historyList+=CalculatorClass.history+"\n";
+                                historyText.setText(CalculatorClass.historyList);
+                                CalculatorClass.history = "";
+                            } else {
+                                resultText.setText(CalculatorClass.history);
+                            }
+                        }
+                    }
+                }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assignment1_simple_calculator/CalculatorClass.java
@@ -0,0 +1,105 @@
+package com.example.assignment1_simple_calculator;
+
+import java.util.ArrayList;
+
+public class CalculatorClass {
+
+    //one record for history
+    public static String history = "";
+
+    //ArrayList to store String history as elements of ArrayList
+    public static ArrayList<String> historyArrayList = new ArrayList<>();
+
+    public static String historyList = "";
+
+    public static boolean isNumber(String s) {
+        switch (s) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean isOperator(String s) {
+        switch (s) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                return true;
+        }
+        return false;
+    }
+
+    public static void push(String symbol) {
+        if (symbol.equals("=")) {
+            historyArrayList.clear();
+            history += symbol;
+            parseStringToArrayList(history);
+        } else {
+            history += symbol;
+        }
+    }
+
+//    static void printArray(ArrayList<String> array) {
+//        for (int i = 0; i < array.size(); i++) {
+//            System.out.println(array.get(i));
+//        }
+//    }
+
+    public static void parseStringToArrayList(String s) {
+        String number = "";
+        String symbol;
+        for (int i = 0; i < s.length(); i++) {
+            symbol = "" + s.charAt(i);
+            if (isNumber(symbol)) {
+                number += symbol;
+            } else {
+                historyArrayList.add(number);
+                historyArrayList.add(symbol);
+                number = "";
+            }
+        }
+    }
+
+    public static void calculate() {
+        double result = Double.parseDouble(historyArrayList.get(0));
+        double operand;
+        String operator ;
+        for (int i = 0; i < historyArrayList.size() - 2; i = i + 2) {
+            operator = historyArrayList.get(i + 1);
+            operand = Double.parseDouble(historyArrayList.get(i + 2));
+            result = simpleCalculate(result, operand, operator);
+        }
+        history = history + result;
+
+    }
+
+    public static double simpleCalculate(double n1, double n2, String oper) {
+        double result = 0;
+        switch (oper) {
+            case "+":
+                result = n1 + n2;
+                break;
+            case "-":
+                result = n1 - n2;
+                break;
+            case "*":
+                result = n1 * n2;
+                break;
+            case "/":
+                result = n1 / n2;
+                break;
+        }
+        return result;
+    }
+}
